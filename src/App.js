04fae445b5802af988e00f0e6054da3f853fa699
new name <new email>
2021-08-17 23:34:10 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./App.css";
+import News from "./Components/News/News";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=eb84e5a5679540a7856e3c9ed6d1cf69"
+      )
+      .then((res) => setArticles(res.data.articles))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav className="nav">
+    <div>
+    Breaking News
     </div>
+    <div>
+    Breaking News
+    </div>
+    <div>
+    Breaking News
+    </div>
+   
+      
+    </nav>
+      <div className="App">
+        {articles.map((article) => (
+          <News className="orvi" article={article}></News>
+        ))}
+      </div>
+    </>
   );
 }
 
