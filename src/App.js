05961981a6fcfc371ce1d4767bd2763a35334574
new name <new email>
2022-01-19 01:02:20 +0600

    new888

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-import "./App.css";
+
 import News from "./Components/News/News";
 
 function App() {
